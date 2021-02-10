212fc1d4007b447f3766e046a01982800ec39545
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-09 21:17:59 -0800

    deploying

--- server.js
+++ server.js
@@ -10,9 +10,18 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Define API routes here
 
+// mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ballyhoo',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
