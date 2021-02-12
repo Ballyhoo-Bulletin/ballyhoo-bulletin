de57246f6c9a3c07da431884732446029dc05922
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-12 09:11:18 -0800

    redirecting to dashboard feed

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
 
+// can check current user by {currentUser && CurrentUser.email or .whatever}
 
 export default function Signup() {
     const passwordConfirmRef = useRef()
@@ -10,6 +12,7 @@ export default function Signup() {
     const { signup } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const history = useHistory()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -22,6 +25,7 @@ export default function Signup() {
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            history.push("/")
         } catch {
             setError("Sign up failed!")
         }
@@ -34,6 +38,7 @@ export default function Signup() {
            <Card>
                <Card.Body>
                    <h2 className="text-center mb-4">Sign Up</h2>
+                   {/* {currentUser.email} */}
                    {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
@@ -53,7 +58,7 @@ export default function Signup() {
                </Card.Body>
            </Card>
            <div className="w-100 text-center-mt-2">
-               Already have an account? Log In 
+               Already have an account? <Link to="/login">Login</Link>
            </div>
         </>
     )
