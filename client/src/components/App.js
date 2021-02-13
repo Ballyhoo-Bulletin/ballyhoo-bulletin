35a42919c9273f7afdb5f9ddcfa4a8bf2dabfa13
Nadine Delaire <ndelaire16@gmail.com>
2021-02-13 13:24:38 -0800

    changing a syntax error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Signup from "./Signup";
+import Signup from "./signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
