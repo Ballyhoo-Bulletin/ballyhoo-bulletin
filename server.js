1934d55873aa8c5eeca3971eb6c34ed94949f1ad
Nadine Delaire <ndelaire16@gmail.com>
2021-02-14 16:03:01 -0800

    adding mongoose information

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ballyhoo',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // Define API routes here
 
 // Send every other request to the React app
