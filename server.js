da9d1a3bc6ef6c97f57dbdb5c7d3e9a6e4123190
Amber Lee <lee.amber.alex@gmail.com>
2021-02-16 10:00:33 -0800

    Commented out unused code, adjusted mongoose connection.

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-// const user = require("./routes/api/user");
-// const trade = require("./routes/api/trade");
+
+
 const app = express();
 
-
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+// Define API routes here
+// Define any API routes before this runs
+
+// const user = require("./routes/api/user");
+// const trade = require("./routes/api/trade");
+
+// app.use("/api/user", user)
+// app.use("/api/trade", trade)
+
+// Send every other request to the React app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/ballyhoo',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
 
-app.use("/api/user", user)
-app.use("/api/trade", trade)
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ballyhoo');
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// );
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
