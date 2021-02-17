f23ccad37191de4fe4978718c8c75a4f697d462e
Nadine Delaire <ndelaire16@gmail.com>
2021-02-16 21:09:18 -0800

    adding some styling, taking out unneeded code

--- client/src/utils/store/history.js
+++
@@ -1,6 +0,0 @@
-import {createBrowserHistory} from "history";
-
-// creates an object that React Router can use that shows what it
-// is an what is was in the past.  Creates a history instance that all the 
-// other files can use. 
-export const history = createBrowserHistory();
