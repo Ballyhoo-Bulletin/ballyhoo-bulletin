6612fcac75c47cd86fffcdae7b676637689c4d54
Amber Lee <lee.amber.alex@gmail.com>
2021-02-17 22:22:40 -0800

    Added code for API routes.

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,36 +1,52 @@
 import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form, Button, Card, Row, Col, Alert } from "react-bootstrap";
+import API from "../utils/API";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
 // can check current user by {currentUser && CurrentUser.email or .whatever}
 
-export default function Signup(props) {
-  const passwordConfirmRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
+export default function Signup() {
   const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
   const history = useHistory();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const cityRef = useRef();
+  const tradeRef = useRef();
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match!");
-    }
+    } 
 
     try {
       setError("");
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
+      API.saveTrade({
+        email: emailRef.current.value,
+        city: cityRef.current.value,
+        trade: tradeRef.current.value,
+      }).then((result) => {
+        console.log("Success!", result);
+  
+      });
+      await signup(emailRef.current.value, passwordRef.current.value)
       history.push("/");
-    } catch {
+      
+    } 
+    catch {
       setError("Sign up failed!");
     }
 
     setLoading(false);
+
+
   }
 
   return (
@@ -41,22 +57,46 @@ export default function Signup(props) {
           {/* {currentUser.email} */}
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
+        <Form.Group id="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" ref={emailRef} required />{" "}
+        </Form.Group>{" "}
+        <Form.Group id="password">
+          <Form.Label>password</Form.Label>
+          <Form.Control type="password" ref={passwordRef} required />{" "}
+        </Form.Group>{" "}
+        <Form.Group id="password-confirm">
+          <Form.Label>Password Confirmation</Form.Label>
+          <Form.Control
+            type="password"
+            ref={passwordConfirmRef}
+            required
+          />{" "}
+        </Form.Group>
+        <Form.Group id="city">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" ref={cityRef} required />{" "}
+        </Form.Group>{" "}
+        <Form.Group controlId="TradeGroups2">
+          <Form.Label>Select Your Trade Skills:</Form.Label>
+          <Form.Control ref={tradeRef} as="select" multiple>
+            <option>Cooking</option>
+            <option>Cleaning</option>
+            <option>Childcare</option>
+            <option>Education</option>
+            <option>Landscaping</option>
+            <option>Mechanical</option>
+            <option>Petcare</option>
+            <option>Technical</option>
+            <option>I will later</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Create Account</Button>
+          </Col>
+        </Form.Group>
+      </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center-mt-2">
