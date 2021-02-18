af882d3b05d1ce5ae4239303fb3552d555dcbd8c
Amber Lee <lee.amber.alex@gmail.com>
2021-02-17 22:22:49 -0800

    Added comments.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all posts
+  // Gets all trades
   getTrade: function () {
     return axios.get("/api/trades");
   },
-  // Gets the post with the given id
+  // Gets the trades with the given id
   // getTrade: function (id) {
-  //   return axios.get("/api/posts/" + id);
+  //   return axios.get("/api/trades/" + id);
   // },
-  // Deletes the post with the given id
+  // Deletes the trades with the given id
   deleteTrade: function (id) {
     return axios.delete("/api/trades/" + id);
   },
-  // Saves a post to the database
+  // Saves a trades to the database
   saveTrade: function (tradeData) {
     return axios.post("/api/trades", tradeData);
   },
