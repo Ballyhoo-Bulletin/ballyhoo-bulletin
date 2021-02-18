fc10514879078eccdd436d3dd9e2b8c3675f38fd
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-17 17:43:08 -0800

    fixing route url

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/api-routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -15,9 +15,9 @@ if (process.env.NODE_ENV === "production") {
 
 
 // Send every other request to the React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ballyhoo");
 //   {
 //     useNewUrlParser: true,
