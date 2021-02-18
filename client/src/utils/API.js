bddfd2729b885467b001cb17e153049c8ab3fd0b
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-17 17:42:34 -0800

    adding export

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default {
   // Gets all posts
   getTrade: function () {
-    return axios.get("/api/post");
+    return axios.get("/api/trades");
   },
   // Gets the post with the given id
-  getTrade: function (id) {
-    return axios.get("/api/posts/" + id);
-  },
+  // getTrade: function (id) {
+  //   return axios.get("/api/posts/" + id);
+  // },
   // Deletes the post with the given id
   deleteTrade: function (id) {
-    return axios.delete("/api/posts/" + id);
+    return axios.delete("/api/trades/" + id);
   },
   // Saves a post to the database
   saveTrade: function (tradeData) {
-    return axios.post("/api/posts", tradeData);
+    return axios.post("/api/trades", tradeData);
   },
 };
