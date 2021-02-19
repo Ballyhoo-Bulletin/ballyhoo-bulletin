b2a848b8b89b0259509010775bc9c77b95f97594
Amber Lee <lee.amber.alex@gmail.com>
2021-02-18 19:31:52 -0800

    Added function to grab all skills.

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -10,6 +10,9 @@ export default function Signup() {
   const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+  
+  console.log(options);
 
   const history = useHistory();
   const emailRef = useRef();
@@ -18,12 +21,22 @@ export default function Signup() {
   const cityRef = useRef();
   const tradeRef = useRef();
 
+  function handleSelectChange(e) {
+    e.preventDefault();
+    // setOptions(e.target.value)
+    if (options.includes(e.target.value)) {
+      setOptions(options.filter(x => x !== e.target.value))
+    } else {
+      setOptions([...options, e.target.value]);
+    }
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match!");
-    } 
+    }
 
     try {
       setError("");
@@ -31,23 +44,19 @@ export default function Signup() {
       API.saveUser({
         email: emailRef.current.value,
         city: cityRef.current.value,
-        trade: tradeRef.current.value,
+        trade: options,
       }).then((result) => {
         console.log("Success!", result);
-  
       });
-      await signup(emailRef.current.value, passwordRef.current.value)
+      await signup(emailRef.current.value, passwordRef.current.value);
       history.push("/");
-      
-    } 
-    catch {
+    } catch {
       setError("Sign up failed!");
     }
 
     setLoading(false);
-
-
   }
+  console.log(tradeRef.current && tradeRef.current.value);
 
   return (
     <>
@@ -57,46 +66,51 @@ export default function Signup() {
           {/* {currentUser.email} */}
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-        <Form.Group id="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" ref={emailRef} required />{" "}
-        </Form.Group>{" "}
-        <Form.Group id="password">
-          <Form.Label>password</Form.Label>
-          <Form.Control type="password" ref={passwordRef} required />{" "}
-        </Form.Group>{" "}
-        <Form.Group id="password-confirm">
-          <Form.Label>Password Confirmation</Form.Label>
-          <Form.Control
-            type="password"
-            ref={passwordConfirmRef}
-            required
-          />{" "}
-        </Form.Group>
-        <Form.Group id="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" ref={cityRef} required />{" "}
-        </Form.Group>{" "}
-        <Form.Group controlId="TradeGroups2">
-          <Form.Label>Select Your Trade Skills:</Form.Label>
-          <Form.Control ref={tradeRef} as="select" multiple>
-            <option>Cooking</option>
-            <option>Cleaning</option>
-            <option>Childcare</option>
-            <option>Education</option>
-            <option>Landscaping</option>
-            <option>Mechanical</option>
-            <option>Petcare</option>
-            <option>Technical</option>
-            <option>I will later</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group as={Row}>
-          <Col sm={{ span: 10, offset: 2 }}>
-            <Button type="submit">Create Account</Button>
-          </Col>
-        </Form.Group>
-      </Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />{" "}
+            </Form.Group>{" "}
+            <Form.Group id="password">
+              <Form.Label>password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />{" "}
+            </Form.Group>{" "}
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                required
+              />{" "}
+            </Form.Group>
+            <Form.Group id="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control type="text" ref={cityRef} required />{" "}
+            </Form.Group>{" "}
+            <Form.Group controlId="TradeGroups2">
+              <Form.Label>Select Your Trade Skills:</Form.Label>
+              <Form.Control
+                value={options}
+                onChange={handleSelectChange}
+                as="select"
+                multiple
+              >
+                <option>Cooking</option>
+                <option>Cleaning</option>
+                <option>Childcare</option>
+                <option>Education</option>
+                <option>Landscaping</option>
+                <option>Mechanical</option>
+                <option>Petcare</option>
+                <option>Technical</option>
+                <option>I will later</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Col sm={{ span: 10, offset: 2 }}>
+                <Button type="submit">Create Account</Button>
+              </Col>
+            </Form.Group>
+          </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center-mt-2">
