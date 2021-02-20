838f273447f3f6f1590c2c445efa8f936e21f69a
Amber Lee <lee.amber.alex@gmail.com>
2021-02-20 08:56:28 -0800

    Added props key.

--- client/src/components/TradeCard/TradeCard.js
+++ client/src/components/TradeCard/TradeCard.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Card, Button, ListGroup} from "react-bootstrap";
+import React from "react";
+import { Card, Button, ListGroup } from "react-bootstrap";
 
+const styles = {
+  text: {
+    textAlign: "center",
+  },
+};
 
-
-export default function TradeCard() {
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <ListGroup variant="flush">
-    <ListGroup.Item>Skills or Item</ListGroup.Item>
-    <ListGroup.Item>What's being traded?</ListGroup.Item>
-  </ListGroup>
-    <Card.Text>
-      Maybe the description or meeting place or whatever goes here?
-    </Card.Text>
-    <Button variant="primary">Claim</Button>
-  </Card.Body>
-</Card>
-        </div>
-    )
+export default function TradeCard(props) {
+  return (
+    <div>
+      <Card style={{ width: "25rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body style={styles.text}>
+          <Card.Title>{props.email}</Card.Title>
+          <ListGroup variant="flush">
+            <ListGroup.Item>I need: {props.need}</ListGroup.Item>
+            <ListGroup.Item>
+              And can trade you for: {props.children}
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Text>
+            Additional info:
+            {props.description}
+          </Card.Text>
+          <Button variant="primary">Claim</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
