7a7833e481701526b78f155cd12529b2e71532d0
Amber Lee <lee.amber.alex@gmail.com>
2021-02-22 00:26:33 -0800

    Added userID and email field.

--- models/trade.js
+++ models/trade.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tradeSchema = new Schema({
+  userID: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
   need: {
     type: String,
   },
