137a6b2aaf57f6b39c104fa993088f59ee94c8c9
Kelsea Fields <68616301+Kfields91@users.noreply.github.com>
2021-02-25 22:01:31 -0800

    Nadine branch (#33)
    * adding in login page
    
    * changing a syntax error
    
    * adding store component and adding js file
    
    * adding packages
    
    * needed to update package.json
    
    * adding checkbox to signup and starting create account component
    
    * taking out create account page and adding to sign up
    
    * adding to sign up form
    
    * adding a style css but still working out how to make it work
    
    * adding font
    
    * styling
    
    * styling
    
    * styling - looking into adding a navbar
    
    * taking out navbar, using a different one that was created
    
    * adding mongoose information
    
    * adding changes so I can pull from develop
    
    * taking out MDB react code
    
    * adding so I can pull new code
    
    * took out a dependency that wasn't needed to get code to work
    
    * adding back in code that got deleted on signup
    
    * adding some styling, taking out unneeded code
    
    * adding changes to pull
    
    * adding new background image and trying a new font
    
    * styling
    
    * styling
    
    * styling
    
    * styling
    
    * cont styling
    
    * taking out unused components
    
    * adding updates that didn't pull
    
    * updated private route to route it correctly
    
    * styling
    
    * styling
    
    * adding to readme
    
    * readme add
    
    * readme add
    
    * readme add
    
    * making layout responsive
    
    * adding changes to pull
    
    * adding to pull
    
    * changes
    
    * readme
    
    Co-authored-by: Nadine Delaire <ndelaire16@gmail.com>

--- client/src/components/HomeDashboard/HomeDashboard.js
+++ client/src/components/HomeDashboard/HomeDashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import API from "../../utils/API";
-import "./style.css";
+// import "./style.css";
 import TradeCard from "../TradeCard/TradeCard";
 
 const HomeDashboard = () => {
@@ -72,11 +72,11 @@ const HomeDashboard = () => {
   return (
     <div className="container">
       <div>
-        <p>Dashboard</p>
+    
       </div>
       <div className="row">
         <div className="col-md-2">
-          <p>thrumup with icon</p>
+          
         </div>
         <div className="col-md-10"></div>
       </div>
